df4ee0fe62eef25e1d508a0084a48b2c13a1f958
Anton Efimenko <reloni@ya.ru>
2019-09-13 16:22:11 +0300

    Initial Commit

---
+++ FinanceTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinanceTracker
+//
+//  Created by Anton Efimenko on 13/09/2019.
+//  Copyright © 2019 Anton Efimenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
